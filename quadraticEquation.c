9c39aad6ba3eebe174ac574836be9e85cf497014
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-11-06 05:40:25 +0100

    update

--- quadraticEquation.c
+++ quadraticEquation.c
@@ -2,20 +2,17 @@
 #include <math.h>
 
 int main(){
-    float a,b,c,rt1,rt2;
-
-    printf("Enter the value of 'a' of your quadratic equation: ");
-    scanf("%f", &a);
-
-    printf("Enter the value of 'b' of your quadratic equation: ");
-    scanf("%f", &b);
-
-    printf("Enter the value of 'c' of your quadratic equation: ");
-    scanf("%f", &c);
-
-    rt1 = (-b + sqrt(b*b-4*a*c))/ (2*a);
-    rt2 = (-b - sqrt(b*b-4*a*c))/ (2*a);
-
-    printf("The roots of your quadratic equation are %f and %f", rt1, rt2);
-
+    double a,b,c,root1,root2;
+    printf(" Please enter a \n");
+    scanf("%lf",&a);
+    printf(" Please enter b \n");
+    scanf("%lf",&b);
+    printf(" Please enter c \n");
+    scanf("%lf",&c);
+    root1 = (-b + sqrt(b*b-4.*a*c) ) / (2.*a);
+    root2 = (-b - sqrt(b*b-4.*a*c) ) / (2.*a);
+    printf("\n First root is %lf ",root1);
+    printf("\n Second root is %lf ",root2);
+    printf("\n ");
     return 0;
+}
